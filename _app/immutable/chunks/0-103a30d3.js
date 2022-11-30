202c287d88931da690962aa7ae5d8de4a5cef8d1
buckldav <buckldav@users.noreply.github.com>
2022-11-30 18:28:57 +0000

    deploy: c42125bac65de74d2135c580faaf8b65da903a53

---
+++ _app/immutable/chunks/0-103a30d3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-d71adb9e.js";const e=!0;export{t as component,e as server};
---
+++ _app/immutable/chunks/3-1ac78c62.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/about/_page.svelte-82ba5a3a.js";export{t as component};
